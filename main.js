87e2886c2e7548c2ea16ac25a4530d9b0d24f523
Patrick McNamara <hello@patrickmcnamara.xyz>
2018-06-13 22:46:18 +0100

    Reworked JavaScript (again, again)
    - Remove all old JavaScript
    - Replace with a clickable element that disables CSS

--- main.js
+++ main.js
@@ -1,27 +1,9 @@
-function getTimestamp() {
-  var t = new Date().toISOString();
-  return t;
-}
-
-function getChecksum() {
-  var s = document.documentElement.innerText;
-  var chk = 0x12345678;
-  var len = s.length;
-  for (var i = 0; i < len; i++) {
-    chk += (s.charCodeAt(i) * (i + 1));
-  }
-  var c = (chk & 0xffffffff).toString(16);
-  return c;
-}
-
-function appendMetaInfo(mi) {
-  var p = document.createElement("p");
-  var tn = document.createTextNode("// " + mi.join(", "))
-  p.appendChild(tn)
-  document.body.appendChild(p);
-}
-
+pr = false
 window.onload = function () {
-  var mi = ["TS: " + getTimestamp(), "CS: " + getChecksum()];
-  appendMetaInfo(mi);
+  document.getElementById("acc").onclick = function () {
+    if (!pr) {
+      pr = true
+      document.querySelector("link").remove()
+    }
+  }
 }
