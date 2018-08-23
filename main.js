188df6067fbca346b17f4cc8e25a9499462eb230
Patrick McNamara <hello@patrickmcnamara.xyz>
2018-08-23 02:11:41 +0100

    Minor update
    - Rewrite into
    - Remove most colour and highlighting
    - Remove CSS stripper
    - Remove unnecessary social links
    - Remove (hopefully) unnecessary meta tags
    - Reformat HTML

--- main.js
+++
@@ -1,9 +0,0 @@
-pr = false
-window.onload = function () {
-  document.getElementById("acc").onclick = function () {
-    if (!pr) {
-      pr = true
-      document.querySelector("link").remove()
-    }
-  }
-}
